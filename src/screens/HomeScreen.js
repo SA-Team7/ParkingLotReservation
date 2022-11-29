744bcfe6e4bb5642c41925da05a84338023aca31
sohyun <kshyun0215@gmail.com>
2022-11-29 17:49:32 +0900

    위치 정보 권한 요청 및 현재 위치 가져오기

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import {StyleSheet, TextInput, TouchableOpacity, View} from 'react-native';
+import {
+  PermissionsAndroid,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import NaverMapView from 'react-native-nmap';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Geolocation from 'react-native-geolocation-service';
+
 const HomeScreen = ({navigation}) => {
+  const [location, setLocation] = React.useState({
+    latitude: 37.5408,
+    longitude: 126.9783881,
+  });
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      await Geolocation.requestAuthorization('whenInUse');
+      getCurrentLocation();
+    } else {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        getCurrentLocation();
+      } else {
+        console.log('Location permission denied');
+      }
+    }
+  };
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({latitude, longitude});
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+  };
+
+  React.useEffect(() => {
+    requestLocationPermission();
+  }, []);
+  console.log(location);
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
@@ -11,7 +57,15 @@ const HomeScreen = ({navigation}) => {
         </TouchableOpacity>
         <TextInput style={styles.input} placeholder="Search" />
       </View>
-      <NaverMapView style={styles.map} />
+      <NaverMapView
+        style={styles.map}
+        showsMyLocationButton={true}
+        center={{
+          zoom: 15,
+          latitude: location.latitude,
+          longitude: location.longitude,
+        }}
+      />
     </View>
   );
 };
